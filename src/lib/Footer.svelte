2991cc3e3965f6c5d081d234c314a22a2a22e7bf
N4IR0 <fv@nairo.de>
2023-01-29 17:34:04 +0100

    add required credit for logo

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -8,6 +8,9 @@
             <div class="d-flex align-items-center">
                 <img src="{Logo}" width="30" alt="Logo">
                 <span class="text-muted me-2 ms-2">© 2023 Florian Voit</span>
+                <span class="text-muted">
+                    - Logo wurde erstellt von <a href="https://www.flaticon.com/de/kostenlose-icons/co2" title="co2 Icons" class="text-muted">wanicon - Flaticon</a>
+                </span>
             </div>
 
             <ul class="navbar-nav mb-lg-0">
